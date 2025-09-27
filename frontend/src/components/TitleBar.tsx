3b098441a11b0f59d45a218848344604c42462a7
Luke Vaillancourt <kufferttv@gmail.com>
2025-09-27 16:23:13 -0400

    Remove TitleBar.tsx

--- frontend/src/components/TitleBar.tsx
+++
@@ -1,8 +0,0 @@
-// frontend/src/components/TitleBar.tsx
-export default function TitleBar() {
-  return (
-    <header className="title-bar">
-      <h1>Precision Advertising: Get more Real Clicks and Real Results</h1>
-    </header>
-  )
-}
